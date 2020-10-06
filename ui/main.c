e414d6777df6ffd4cd40f6f56b593f97ed019e62
Lakindu Akash <lakinduakash@gmail.com>
2020-10-07 00:39:12 +0530

    Update .gitignore

---
+++ ui/main.c
@@ -0,0 +1,5 @@
+//
+// Created by lakindu on 2020-08-09.
+//
+
+#include "main.h"
---
+++ ui/main.h
@@ -0,0 +1,8 @@
+//
+// Created by lakindu on 2020-08-09.
+//
+
+#ifndef ASUS_WMI_SCREENPAD_MAIN_H
+#define ASUS_WMI_SCREENPAD_MAIN_H
+
+#endif //ASUS_WMI_SCREENPAD_MAIN_H
