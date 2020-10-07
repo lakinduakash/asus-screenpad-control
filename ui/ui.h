4b71c8a33350e74aeb1ed7fa2bf0402b16b46ab7
Lakindu Akash <lakinduakash@gmail.com>
2020-10-07 19:09:14 +0530

    Add functinality to turn off screenpad

--- ui/ui.h
+++ ui/ui.h
@@ -6,10 +6,11 @@
 int initUi(int argc, char *argv[]);
 int get_interpolated_value_from_file(int value);
 int get_interpolated_value_from_slider(int value);
-int get_brightness_level();
-void set_brightness_level(int value);
+int get_brightness_level(void);
+int set_brightness_level(int value);
 void on_slider_value_changed(GtkRange *range, gpointer data);
 void show_file_open_error(void);
 void setup_ui(void);
+void on_turn_off_switch_changed(GtkSwitch *gtk_switch,gboolean state,gpointer data);
 
 #endif //UI_H
